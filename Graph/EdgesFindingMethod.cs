59cccd551a16a72a884bd45516e1e22a9ff4e5cf
Syed Muhammad Muhsin karim <37833481+SyedMuhammadMuhsinKarim@users.noreply.github.com>
2019-01-11 11:08:04 +0500

    Update EdgesFindingMethod.cs

--- Graph/EdgesFindingMethod.cs
+++ Graph/EdgesFindingMethod.cs
@@ -1,8 +1,10 @@
 /* FIND NUMBER OF EDGES BETWEEN VERTICES IN GRAPH (MATRIX ADGENCY)
  * Input: 2D Matrix (ARRAY);
- * Output: No of Edges Conected Between two vertices 
+ * Output: No of Edges Conected Between two vertices
  */
-static int GraphEdge(int[,] array)
+
+//NonDirected Graph
+static int NonDirectedGraphEdge(int[,] array)
 {
     int count = 0;
     for (int i = 0; i < array.GetLength(0); i++)
@@ -16,4 +18,21 @@ static int GraphEdge(int[,] array)
         }
     }
     return count / 2;
+}
+
+//Directed Graph
+static int DirectedGraphEdge(int[,] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == array[j, i] && (array[i, j] == 1 && array[j, i] == 1))
+            {
+                count = count + 1;
+            }
+        }
+    }
+    return count;
 }
