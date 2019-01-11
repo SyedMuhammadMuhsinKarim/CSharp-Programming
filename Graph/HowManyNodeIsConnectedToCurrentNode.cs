ea7e5920b3be314ef60eda011db66e907b11ccf7
Syed Muhammad Muhsin karim <37833481+SyedMuhammadMuhsinKarim@users.noreply.github.com>
2019-01-11 11:26:57 +0500

    Update HowManyNodeIsConnectedToCurrentNode.cs

--- Graph/HowManyNodeIsConnectedToCurrentNode.cs
+++ Graph/HowManyNodeIsConnectedToCurrentNode.cs
@@ -1,31 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication2
-{
-    class Program
-    {
-        static void Main(string[] args)
+static void NomberOfNodeIsConnected(int[,] a)
         {
-            //Change Graph to Matrix
-            int[,] a = new int[6, 6] { { 0, 0, 1, 1, 0, 0},
-                                       { 1, 0, 0, 0, 0, 0},
-                                       { 0, 0, 0, 1, 1, 0},
-                                       { 0, 0, 0, 0, 0, 1},
-                                       { 0, 0, 0, 0, 0, 1},
-                                       { 0, 0, 0, 0, 0, 0}
-                                      };
-            Connection(a);
-            Console.Read();
-        }
-        
-        /* How Many Nodes is Connected to Current Node (Matrix Adjency) */
-        static void Connection(int[,] a)
-        {
-            int r = a.GetLength(0);
+            int r = 6;
             int c = a.GetLength(1);
             int[] arr = new int[r];
 
@@ -43,11 +18,9 @@ namespace ConsoleApplication2
                 }
                 i++;
             }
-
+            Console.WriteLine();
             for (int k = 0; k < r; k++)
             {
                 Console.WriteLine("Node {0} connected to {1} nodes", k, arr[k]);
             }
         }
-    }
-}
